f845e6a75ad47049245782ca0a525222927a5bca
AlphaEliasPY <84801328+AlphaEliasPY@users.noreply.github.com>
2021-09-26 10:39:17 -0400

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -31,7 +31,7 @@ def help_user(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.HELP_USER,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="👤 Soporte", url="https://t.me/DKzippO")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="👤 Soporte", url="https://t.me/AndroidCave")]]),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
@@ -47,11 +47,9 @@ def send_start(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                  InlineKeyboardButton(text="👤 Soporte", url="https://t.me/DKzippO"), 
-                  InlineKeyboardButton(text="📢 Canales", url="https://t.me/proandroid2")
+                  InlineKeyboardButton(text="👤 Soporte", url="https://AndroidCave"), 
                 ],
-                [
-                  InlineKeyboardButton(text="🎓 Cursos Pro Android", url="https://t.me/joinchat/VDY6seEnkeKdZNRM")
+               
                 ]
             ]
         ),
